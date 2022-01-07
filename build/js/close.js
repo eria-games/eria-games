416503d0196e8b4e36c9cde1036703cb83cdbf8c
eria-games <97305017+eria-games@users.noreply.github.com>
2022-01-07 22:05:47 +0500

    Delete build directory

--- build/js/close.js
+++
@@ -1,9 +0,0 @@
-// Pop-up block
-$(function () {
-    $('.pop-up').hide();
-    $('.pop-up').fadeIn(100);
-    $('#close , #agree').click(function (e) {
-      $('.pop-up').fadeOut(100);
-      e.stopPropagation();
-    });
-  });
